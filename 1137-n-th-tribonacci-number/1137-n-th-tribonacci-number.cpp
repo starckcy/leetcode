6f8728aac075a6d8a51cb81b4da7b2cbb06f72a9
Ayush Kumar Rai <thisisayushkumarrai@gmail.com>
2026-08-06 15:15:31 +0530

    Time: 0 ms (100%), Space: 7.8 MB (69.96%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,13 +3,13 @@ public:
     int tribonacci(int n) {
         if(n == 0) return 0;
         else if(n == 1 || n == 2) return 1;
-        vector<int> dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int a=0, b=1, c=1, d;
         for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            d = a+b+c;
+            a=b;
+            b=c;
+            c=d;
         }
-        return dp[n];
+        return c;
     }
 };
