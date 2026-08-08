bb87d54709b15a4fc9ca820e7819685984ff42da
Ayush Kumar Rai <thisisayushkumarrai@gmail.com>
2026-08-09 00:29:28 +0530

    Time: 0 ms (100%), Space: 10.6 MB (47.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int solve(vector<int>& nums, int i, vector<int>& dp){
-        if(i >= nums.size()) return 0;
+        if(i == nums.size()-1) return nums[i];
+        if(i == nums.size()-2) return max(nums[i], nums[i+1]);
         if(dp[i] != -1) return dp[i];
         return dp[i] = max(solve(nums, i+2, dp) + nums[i], solve(nums, i+1, dp));
     }
