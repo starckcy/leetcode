8f9638ccfdfb192c80954fc4c8099f4ca11cfe90
Ayush Kumar Rai <thisisayushkumarrai@gmail.com>
2026-08-09 00:42:17 +0530

    Time: 0 ms (100%), Space: 10.3 MB (72.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, vector<int>& dp){
-        if(i == nums.size()-1) return nums[i];
-        if(i == nums.size()-2) return max(nums[i], nums[i+1]);
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = max(solve(nums, i+2, dp) + nums[i], solve(nums, i+1, dp));
+    vector<int> dp;
+    // int rec(vector<int>& nums, int i, int e){
+    //     if(i > e) return 0;
+    //     return max(rec(nums, i+2, e) + nums[i], rec(nums, i+1, e));
+    // }
+
+    // int ftd(vector<int>& nums, int i){
+    //     if(i == nums.size()-1) return nums[i];
+    //     if(i == nums.size()-2) return max(nums[i], nums[i+1]);
+    //     if(dp[i] != -1) return dp[i];
+    //     return dp[i] = max(ftd(nums, i+2) + nums[i], ftd(nums, i+1));
+    // }
+
+    int fbu(vector<int>& nums){
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        dp.clear();
+        dp.resize(n);
+
+        dp[n-1] = nums[n-1];
+        dp[n-2] = max(nums[n-1], nums[n-2]);
+        for(int i=n-3;i>=0;i--){
+            dp[i] = max(dp[i+2] + nums[i], dp[i+1]);
+        }
+
+        return dp[0];
     }
+    
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return solve(nums, 0, dp);
+        dp.clear();
+        dp.resize(105, -1);
+        return fbu(nums);
     }
 };
