796016ad36d20e4fb1b59d0fc7816ab2c02e5993
Ayush Kumar Rai <thisisayushkumarrai@gmail.com>
2026-08-15 16:31:33 +0530

    Time: 0 ms (100%), Space: 14 MB (23.6%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -3,21 +3,25 @@ public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<int>> ans(m, vector<int>(n));
 
+        vector<vector<int>> pref(m+1, vector<int>(n+1, 0));
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                pref[i][j] = mat[i-1][j-1] + pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1];
+            }
+        }
+
+        vector<vector<int>> ans(m, vector<int>(n));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
+                int r1 = max(0, i-k);
+                int c1 = max(0, j-k);
+                int r2 = min(i+k, m-1);
+                int c2 = min(j+k, n-1);
 
-                int sum=0;
-                for(int r = i-k; r <= i+k; r++){
-                    for(int c = j-k; c <= j+k; c++){
-                        if(r >= 0 && r < m && c >= 0 && c < n){
-                            sum += mat[r][c];
-                        }
-                    }
-                }
+                r1++; c1++; r2++; c2++;
 
-                ans[i][j] = sum;
+                ans[i][j] = pref[r2][c2] - pref[r1-1][c2] - pref[r2][c1-1] + pref[r1-1][c1-1];
             }
         }
 
