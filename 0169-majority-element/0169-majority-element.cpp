bccb1cb2a34921406ab818a0dd28781eaf63ad34
Ayush Kumar Rai <thisisayushkumarrai@gmail.com>
2026-08-26 01:12:41 +0530

    Time: 1 ms (35.45%), Space: 28.4 MB (14.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,7 +4,9 @@ public:
         unordered_map<int, int> mp;
         for(int x : nums){
             mp[x]++;
-            if(mp[x] > nums.size()/2) return x;
+        }
+        for(auto x : mp){
+            if(x.second > nums.size()/2) return x.first;
         }
         return 0;
     }
