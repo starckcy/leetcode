c57edcc1f64c06913e50331270e123d2f3a127aa
Ayush Kumar Rai <thisisayushkumarrai@gmail.com>
2026-09-05 15:12:19 +0530

    Time: 0 ms (100%), Space: 31.7 MB (6.17%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    int fmx(int i, vector<int> nums){
-        int j=0, mx = INT_MIN;
-        while(j <= i){
-            mx = max(mx, nums[j]);
-            j++;
-        }
-        return mx;
-    }
-    int fmn(int i, vector<int> nums){
-        int j=i, mn = INT_MAX;
-        while(j <= nums.size()-1){
-            mn = min(mn, nums[j]);
-            j++;
-        }
-        return mn;
-    }
     
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        int i=0;
-        while(i<n){
-            int mx = fmx(i, nums);
-            int mn = fmn(i, nums);
-            
-            if(mx-mn <= k){
+        vector<int> prefmx(n);
+        vector<int> suffmn(n);
+
+        prefmx[0] = nums[0];
+        for(int i=1;i<n;i++){
+            prefmx[i] = max(prefmx[i-1], nums[i]);
+        }
+
+        suffmn[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            suffmn[i] = min(suffmn[i+1], nums[i]);
+        }
+
+        for(int i=0; i<n; i++){
+            if(prefmx[i] - suffmn[i] <= k){
                 return i;
             }
-            i++;
         }
+
         return -1;
     }
 };
