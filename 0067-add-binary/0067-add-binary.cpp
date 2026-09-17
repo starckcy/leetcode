463f1b3b4caa83e2c4f0c94727f7e0d7fed91b22
Ayush Kumar Rai <thisisayushkumarrai@gmail.com>
2026-09-18 01:10:46 +0530

    Time: 0 ms (100%), Space: 8.7 MB (96.55%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans;
+        int carry = 0;
+        int i = a.size()-1;
+        int j = b.size()-1;
+
+        while (i >= 0 || j >= 0 || carry) {
+        if (i >= 0){
+            carry += a[i] - '0';
+            i--;
+        }
+        if (j >= 0){
+            carry += b[j] - '0';
+            j--;
+        }
+        ans += carry % 2 + '0';
+        carry /= 2;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
